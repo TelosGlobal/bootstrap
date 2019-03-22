ed3eb988dadc19afb7c5ffd06e0c9cb951567d8e
Kevin Quaintance <kevin@quaintance.net>
2019-03-21 22:48:15 -0700

    Update init_telos.sh

--- init_telos.sh
+++ init_telos.sh
@@ -29,18 +29,26 @@ then
 	echo "Installing required software...."
 	apt install software-properties-common git jq pigz ntp python-pip python3-pip zfsutils-linux -y 
 	apt install salt-minion schedtool stress cpufrequtils lm-sensors linux-tools-generic -y
-
+	apt-get update -y
+	apt-get upgrade -y
+	
 	echo "Setting up ntp...."
 	ntpq -p
 
 	#Change hostname
-	echo "Setting hostname to $1...."
-	hostnamectl set-hostname $1
-	vi /etc/hosts
-	cd /
+	read -p "Set Hostname? (y/n): " confirm
+	if [ $confirm == "Y" ] || [ $confirm == "y" ]
+	then
+	    echo "Setting hostname to $1...."
+	    hostnamectl set-hostname $1
+	    vi /etc/hosts
+	    cd /
+	else
+	    echo "Hostname change cancelled."
+	fi
 
-	read -p "Install Salt Minion? (Y/N): " confirm
-	if [ $confirm == "Y"]|| [ $confirm == "y"]
+	read -p "Install Salt Minion? (y/n): " confirm
+	if [ $confirm == "Y" ] || [ $confirm == "y" ]
 	then
 	    cd /etc/salt
 	    echo "Copy these variables:"
@@ -56,38 +64,54 @@ then
 	    echo "Salt setup cancelled."
 	fi
 
-        echo "ZFS setup ..."
-	parted -l
-	read -p "Enter first raw disk (sdc, sdd, etc): " disk1
-	read -p "Enter second raw disk (sdc, sdd, etc): " disk2
-	read -p "Continue? (Y/N): " confirm
-	if [ $confirm == "Y"]|| [ $confirm == "y"]
-	then
-	    echo "Creating pool and filesystem ..."
-	    zpool create eosio mirror $DISK1 $DISK2
-	    zfs create -o mountpoint=/ext eosio/ext
-	    zpool list
-	    zpool status
-	    zfs list
+	zfs list
+	read -p "Configure ZFS? (y/n): " confirm
+	if [ $confirm == "Y" ] || [ $confirm == "y" ]
+	then        
+	    echo "ZFS setup ..."
+	    parted -l
+	    read -p "Enter first raw disk (sdc, sdd, etc): " disk1
+	    read -p "Enter second raw disk (sdc, sdd, etc): " disk2
+	    read -p "Continue? (y/n): " confirm
+	    if [ $confirm == "Y" ] || [ $confirm == "y" ]
+	    then
+	        echo "Creating pool and filesystem ..."
+	        zpool create eosio mirror $DISK1 $DISK2
+	        zfs create -o mountpoint=/ext eosio/ext
+	        zpool list
+	        zpool status
+	        zfs list
+	    else
+	        echo "ZFS Setup cancelled."
+	    fi
 	else
-	    echo "ZFS Setup cancelled."
+	    echo "ZFS configuration cancelled."
 	fi
 
-	chown -R telosuser /ext
-	cd /ext
-	mkdir $2
-	chown -R telosuser $2
-	ln -s /ext/$2 /ext/telos-build
+	read -p "Install EOSIO? (y/n): " confirm
+	if [ $confirm == "Y" ] || [ $confirm == "y" ]
+	then        
+		chown -R telosuser /ext
+		cd /ext
+		mkdir $2
+		chown -R telosuser $2
+		ln -s /ext/$2 /ext/telos-build
 	
-	#Install Eosio
-	echo "Installing EOSIO Version: "$2
-	cd /ext/telos-build
-	wget 'https://github.com/EOSIO/eos/releases/download/v1.7.0/eosio_1.7.0-1-ubuntu-16.04_amd64.deb'
-	apt install ./eosio_1.7.0-1-ubuntu-16.04_amd64.deb
-	nodeos -v
-
-	read -p "Install Nagios? (Y/N): " confirm
-	if [ $confirm == "Y"]|| [ $confirm == "y"]
+		#Install Eosio
+		echo "Installing EOSIO Version: "$2
+		cd /ext/telos-build
+		sudo add-apt-repository "deb http://security.ubuntu.com/ubuntu xenial-security main" -y
+    		sudo apt-get update -y
+    		sudo apt-get install libicu55 -y
+		wget 'https://github.com/EOSIO/eos/releases/download/v1.7.0/eosio_1.7.0-1-ubuntu-16.04_amd64.deb'
+		apt install ./eosio_1.7.0-1-ubuntu-16.04_amd64.deb
+		nodeos -v
+	else
+	    echo "EOSIO install cancelled."
+	fi
+	
+	read -p "Install Nagios? (y/n): " confirm
+	if [ $confirm == "Y" ] || [ $confirm == "y" ]
 	then
 	    cd /tmp
 	    wget https://assets.nagios.com/downloads/nagiosxi/agents/linux-nrpe-agent.tar.gz
@@ -104,7 +128,7 @@ then
 
 else
     echo "Check arguments!"
-    echo "Usage:  ./init_telos_server.sh [hostname] [git branch tag]"
+    echo "Usage:  ./init_telos_server.sh [hostname] [eosio version]"
     echo "For correct tag, see: https://github.com/Telos-Foundation/telos"
 	echo "Nothing to do.  bye."
 fi
