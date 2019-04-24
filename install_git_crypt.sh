78512d7fd6ea41c96c3f889438dfcea5276b6434
kquainta <q@telosglobal.io>
2019-04-24 23:10:26 +0000

    fixed install_git

--- install_git_crypt.sh
+++ install_git_crypt.sh
@@ -7,7 +7,13 @@ make
 make install PREFIX=/usr/local
 cd /root/bootstrap/
 echo "git-crypt installed.  Copy key file here and run:"
+echo ""
 echo "git-crypt unlock cryptkey"
+echo ""
+echo "Notes:"
+echo "If needed to manually decrypt a file, use:"
+echo "cat overly-encrypted-file | git-crypt smudge > unencrypted-version"
+echo ""
 echo "DELETE cryptkey file when done!"
 
 
