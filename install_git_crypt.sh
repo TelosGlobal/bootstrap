e18bfb98b25057843a74863acb929b31e6c2c6c2
kquainta <q@telosglobal.io>
2019-04-24 21:46:24 +0000

    added install_git_crypt

---
+++ install_git_crypt.sh
@@ -0,0 +1,12 @@
+#This script installs git-crypt
+
+cd /tmp
+git clone https://github.com/AGWA/git-crypt.git
+cd git-crypt
+make
+make install PREFIX=/usr/local
+cd /root/bootstrap/
+echo "git-crypt installed.  Copy key file here and run:"
+echo "git-crypt unlock cryptkey"
+
+
