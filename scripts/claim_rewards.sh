349033bc532a5b2aa561260de7a47626665ddb7e
Kevin Quaintance <kevin@quaintance.net>
2019-04-24 13:25:17 -0700

    Update claim_rewards.sh

--- scripts/claim_rewards.sh
+++ scripts/claim_rewards.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 date
+. .secret
+
 #Unlock wallet
-/ext/telos/cleos wallet unlock -n claimer --password PW5K2jiWEAVj2coHrDPQSjQbZSGUs27CS2rNZPckLFjZPibHns7z1
+/ext/telos/cleos wallet unlock -n claimer --password $CLAIM_WALLET_KEY
+
 #Claim
 /ext/telos/cleos push action eosio claimrewards '{"owner":"telosglobal1"}' -p telosglobal1@claimer
 /ext/telos/cleos wallet lock -n claimer
