38e92d001f79d634df1915a05c6b4094c4dfe505
Kevin Quaintance <kevin@quaintance.net>
2019-04-24 13:31:01 -0700

    Update send2chain.sh

--- scripts/send2chain.sh
+++ scripts/send2chain.sh
@@ -7,21 +7,17 @@
 #  Sends coin to Chainrift $CRACCT (using $MEMO) 
 #  Locks wallet
 
-ACCT="telosglobal1"
-CRACCT="chainriftcom"
-MEMO="28WMLDJFL5"
-WALLET="trx"
-KEY="PW5K2jiWEAVj2coHrDPQSjQbZSGUs27CS2rNZPckLFjZPibHns7z1"
+. .secret
+
 AMT=`/ext/telos/cleos get account $ACCT -j | jq '.core_liquid_balance'`
 AMTT=${AMT%.*}
 COIN=${AMTT:1}
 
 if [ $COIN -gt 0 ] 
 then
-    /ext/telos/cleos wallet unlock -n $WALLET --password $KEY
+    /ext/telos/cleos wallet unlock -n $SENDWALLET --password $SENDKEY
     /ext/telos/cleos push action eosio.token transfer '{"from":"$ACCT","to":"$CRACCT","quantity":"$COIN.0000 TLOS","memo":"$MEMO"}' -p "$ACCT@transfer"
-    /ext/telos/cleos wallet lock -n $WALLET
+    /ext/telos/cleos wallet lock -n $SENDWALLET
 else
     echo "Liquid balance is less than 1.  Balance is $AMT.  Bye."
-
 fi
