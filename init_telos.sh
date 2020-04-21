7322fc359300911aa40b8c8b8e37f195a92a7704
root <root@usmainnetnyprdr01.local.lan>
2020-04-21 18:21:28 -0400

    updated to 2.0.5

--- init_telos.sh
+++ init_telos.sh
@@ -102,7 +102,7 @@ read -p "Install EOSIO? (y/n): " confirm
 if [ $confirm == "Y" ] || [ $confirm == "y" ]
 then        
     echo "Which EOSIO version? "
-    echo "  1:  v2.0.4"
+    echo "  1:  v2.0.5"
     echo "  2:  v1.8.12"
     read -p "Select (1) or (2): " instVer
     if [ $instVer == "1" ] || [ $instVer == "2" ]
@@ -128,7 +128,7 @@ then
         sudo apt-get update -y
         if [ $instVer == "1" ]
         then
-            wget 'https://github.com/EOSIO/eos/releases/download/v2.0.4/eosio_2.0.4-1-ubuntu-18.04_amd64.deb'
+            wget 'https://github.com/EOSIO/eos/releases/download/v2.0.5/eosio_2.0.4-1-ubuntu-18.04_amd64.deb'
             apt install ./eosio_2.0.4-1-ubuntu-18.04_amd64.deb
         else
             wget 'https://github.com/EOSIO/eos/releases/download/v1.8.12/eosio_1.8.12-1-ubuntu-18.04_amd64.deb'
@@ -149,8 +149,8 @@ then
         sudo chown -R telosuser /usr/opt/eosio/
         if [ $instVer == "1" ]
         then
-            ln -s /usr/opt/eosio/2.0.4/bin/nodeos /ext/telos/nodeos
-            ln -s /usr/opt/eosio/2.0.4/bin/cleos /ext/telos/cleos
+            ln -s /usr/opt/eosio/2.0.5/bin/nodeos /ext/telos/nodeos
+            ln -s /usr/opt/eosio/2.0.5/bin/cleos /ext/telos/cleos
 	else    
             ln -s /usr/opt/eosio/1.8.12/bin/nodeos /ext/telos/nodeos
             ln -s /usr/opt/eosio/1.8.12/bin/cleos /ext/telos/cleos
