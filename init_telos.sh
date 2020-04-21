fb12de5fdcaaf920b992edd6396dda1b350392b0
root <root@usmainnetnyprdr01.local.lan>
2020-04-21 18:25:08 -0400

    fixed 2.0.5 link

--- init_telos.sh
+++ init_telos.sh
@@ -128,7 +128,7 @@ then
         sudo apt-get update -y
         if [ $instVer == "1" ]
         then
-            wget 'https://github.com/EOSIO/eos/releases/download/v2.0.5/eosio_2.0.4-1-ubuntu-18.04_amd64.deb'
+            wget 'https://github.com/EOSIO/eos/releases/download/v2.0.5/eosio_2.0.5-1-ubuntu-18.04_amd64.deb'
             apt install ./eosio_2.0.4-1-ubuntu-18.04_amd64.deb
         else
             wget 'https://github.com/EOSIO/eos/releases/download/v1.8.12/eosio_1.8.12-1-ubuntu-18.04_amd64.deb'
