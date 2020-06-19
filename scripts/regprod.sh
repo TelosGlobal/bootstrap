a58a63eb54561fc38cb8655d9b39004da05f4c59
kquainta <kevin@quaintance.net>
2020-06-19 14:47:24 -0700

    added regprod and unregprod scripts

---
+++ scripts/regprod.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+/ext/telos/cleos wallet unlock -n default
+/ext/telos/cleos system regproducer telosglobal1 EOS64HW2FWJ3M2DEmiH19JGHLt8wFqhWKWc3KCeMSsdENmrhMVUGT https://telosglobal.io 840
+/ext/telos/cleos wallet lock -n default
+
