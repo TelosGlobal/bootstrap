f759035cd014ef0ef4a795811fee77b781e58287
kquainta <kevin@quaintance.net>
2020-06-19 15:19:44 -0700

    fixed key on regprod

--- scripts/regprod.sh
+++ scripts/regprod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 /ext/telos/cleos wallet unlock -n default
-/ext/telos/cleos system regproducer telosglobal1 EOS64HW2FWJ3M2DEmiH19JGHLt8wFqhWKWc3KCeMSsdENmrhMVUGT https://telosglobal.io 840
+/ext/telos/cleos system regproducer telosglobal1 EOS7LAUhhdYr6CgW9TFMSYLxjv83h44xVbewfCS8CK66BinxLjymi https://telosglobal.io 840
 /ext/telos/cleos wallet lock -n default
 
