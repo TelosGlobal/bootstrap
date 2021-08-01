289e66e66ab961ac23c9cff1f4a965c86235d793
kquainta <kevin@quaintance.net>
2021-08-01 09:06:39 -0700

    added telos_envs.sh file

---
+++ telos_envs.sh
@@ -0,0 +1,22 @@
+#Sets Telos specific environment variables
+#Place this file in /etc/profile.d/
+
+alias ls='ls --color=auto'
+alias ll='ls -al --color=auto'
+
+#Claimer Wallet ENVs
+WALLET_PWD=PW5JEEJMwiLikzjBodNhfNVtJJhmxJXybYux2ko9h4EFC9skBTqJC
+export WALLET_PWD
+
+#Send2Exchange ENVs
+SEND2_WALLET=trx
+export SEND2_WALLET
+SEND2_FRACCT=telosglobal1
+export SEND2_FRACCT
+SEND2_WALLETKEY=PW5K4iwwbpnG4sHmY2BfMicvnoC9VztZYBTUgFzLyLDR3oEWaRWwf
+export SEND2_WALLETKEY
+SEND2_TOACCT=probitandtls
+export SEND2_TOACCT
+SEND2_MEMO=27458825
+export SEND2_MEMO
+
