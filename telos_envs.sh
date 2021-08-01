05d3bc70d2ba330af5f416e41317b0906ef6a475
kquainta <kevin@quaintance.net>
2021-08-01 09:09:11 -0700

    updated telos_envs.sh

--- telos_envs.sh
+++ telos_envs.sh
@@ -5,7 +5,7 @@ alias ls='ls --color=auto'
 alias ll='ls -al --color=auto'
 
 #Claimer Wallet ENVs
-WALLET_PWD=PW5JEEJMwiLikzjBodNhfNVtJJhmxJXybYux2ko9h4EFC9skBTqJC
+WALLET_PWD=NOTSETUP
 export WALLET_PWD
 
 #Send2Exchange ENVs
@@ -13,10 +13,10 @@ SEND2_WALLET=trx
 export SEND2_WALLET
 SEND2_FRACCT=telosglobal1
 export SEND2_FRACCT
-SEND2_WALLETKEY=PW5K4iwwbpnG4sHmY2BfMicvnoC9VztZYBTUgFzLyLDR3oEWaRWwf
+SEND2_WALLETKEY=NOTSETUP
 export SEND2_WALLETKEY
 SEND2_TOACCT=probitandtls
 export SEND2_TOACCT
-SEND2_MEMO=27458825
+SEND2_MEMO=NOTSETUP
 export SEND2_MEMO
 
