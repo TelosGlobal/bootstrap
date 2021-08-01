d04aac2f3c899cc7b7003890774e0ad5888370cf
kquainta <kevin@quaintance.net>
2021-07-31 18:42:07 -0700

    pulling snaps from eosarabia

--- scripts/getsnap.sh
+++ scripts/getsnap.sh
@@ -2,15 +2,15 @@
 
 echo "Getting latest MAINNET snapshot..."
 cd /ext/telos/snapshots
-wget https://eosmetal.io/snapshots/telos/latest.tar.gz
+wget http://cdn.eosarabia.net/snapshots/telosm/latest.tar.gz
 
 echo "Unpacking the files..."
 tar -xzvf latest.tar.gz
 rm latest.tar.gz
-cd opt/telos*/data-dir/snapshots/
+cd mnt/tmainnet/
 mv snapshot* /ext/telos/snapshots/snapshot.bin
 cd /ext/telos/snapshots/
-rm -Rf opt/
+rm -Rf mnt/
 
 echo "Done.  Don't forget to empty your blocks/ and state/ folders then run ./snapstart.sh"
 
