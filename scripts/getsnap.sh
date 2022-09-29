1c6898981617d9187b406947237f6e2750dd342f
Kevin Quaintance <kevin@quaintance.net>
2022-09-29 07:08:47 +0000

    switched to greymass for snapshots

--- scripts/getsnap.sh
+++ scripts/getsnap.sh
@@ -2,15 +2,12 @@
 
 echo "Getting latest MAINNET snapshot..."
 cd /ext/telos/snapshots
-wget http://cdn.eosarabia.net/snapshots/telosm/latest.tar.gz
+wget https://snapshots.greymass.network/telos/latest.tar.gz
 
 echo "Unpacking the files..."
 tar -xzvf latest.tar.gz
 rm latest.tar.gz
-cd mnt/tmainnet/
-mv snapshot* /ext/telos/snapshots/snapshot.bin
-cd /ext/telos/snapshots/
-rm -Rf mnt/
+cd /ext/telos/
 
 echo "Done.  Don't forget to empty your blocks/ and state/ folders then run ./snapstart.sh"
 
