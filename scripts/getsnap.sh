d78be794c3fb5892ac7230ef4a5b252a24d9c92b
Kevin Quaintance <kevin@quaintance.net>
2024-01-21 13:28:51 -0800

    updated getsnap.sh

--- scripts/getsnap.sh
+++ scripts/getsnap.sh
@@ -2,12 +2,22 @@
 
 echo "Getting latest MAINNET snapshot..."
 cd /ext/telos/snapshots
-wget https://snapshots.greymass.network/telos/latest.tar.gz
+
+if [[ $(hostname) == *"testnet"* ]]
+then
+  echo "I'm a testnet node...fetching testnet snap."
+  ##TESTNET
+  wget https://snapshots.eosnation.io/telostest-v6/latest
+else
+  #MAINNET
+  wget https://snapshots.eosnation.io/telos-v6/latest
+fi
+echo
 
 echo "Unpacking the files..."
-tar -xzvf latest.tar.gz
-rm latest.tar.gz
-cd /ext/telos/
+zstd --rm latest
+rm snapshot.bin
+mv latest.zst snapshot.bin
 
 echo "Done.  Don't forget to empty your blocks/ and state/ folders then run ./snapstart.sh"
 
