765e8f0c1f26be00fcb3099ff79ea4525575bb95
kquainta <kevin@quaintance.net>
2024-01-22 12:29:03 -0800

    fixed zstd decompress on getsnap.sh

--- scripts/getsnap.sh
+++ scripts/getsnap.sh
@@ -7,17 +7,18 @@ if [[ $(hostname) == *"testnet"* ]]
 then
   echo "I'm a testnet node...fetching testnet snap."
   ##TESTNET
-  wget https://snapshots.eosnation.io/telostest-v6/latest
+  wget -O latest.zst https://snapshots.eosnation.io/telostest-v6/latest
 else
   #MAINNET
-  wget https://snapshots.eosnation.io/telos-v6/latest
+  wget -O latest.zst https://snapshots.eosnation.io/telos-v6/latest
 fi
 echo
 
 rm snapshot.bin
 echo "Unpacking the files..."
-zstd -o snapshot.bin --rm latest
-#mv latest.zst snapshot.bin
+zstd -d latest.zst
+rm latest.zst
+mv latest snapshot.bin
 
 echo "Done.  Don't forget to empty your blocks/ and state/ folders then run ./snapstart.sh"
 
